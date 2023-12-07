bc48ed0254f4479d52ffd2ceeb5644108c04852c
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2023-12-07 19:45:37 +0100

    Lade till en logga i headern och footern, gjorde så att posten man skapar hamnar längst upp på sidan, lade in reactions variablen och gjorde även den interaktiv, "snyggade" till koden, ändrade lite styling och döpte om sidan.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let postContainer = document.getElementById("post-container");
 let postsTitle = document.getElementById("posts-title");
 let postsArray = [];
-
-
+let inputTitle = document.getElementById("input-title");
+let inputBody = document.getElementById("input-body");
+let inputTags = document.getElementById("input-tags");
+let inputBtn = document.getElementById("input-btn");
 
 // Hämtar alla posts
 function getPosts(callback) {
@@ -13,78 +15,76 @@ function getPosts(callback) {
     });
 }
 
-
-let inputTitle = document.getElementById("input-title");
-let inputBody = document.getElementById("input-body");
-let inputTags = document.getElementById("input-tags");
-let inputBtn = document.getElementById("input-btn");
-
-inputBtn.addEventListener("click", function(){
-  let divContainer = document.createElement("div");
-    divContainer.classList.add("posts");
-    postContainer.append(divContainer);
-
-    let titleDiv = document.createElement("div");
-    titleDiv.classList.add("title");
-    divContainer.append(titleDiv);
-
-    let h2 = document.createElement("h2");
-    titleDiv.append(h2);
-
-    let pBody = document.createElement("p");
-    pBody.classList.add("content");
-    divContainer.append(pBody);
-
-    let tagDiv = document.createElement("div");
-    tagDiv.classList.add("div-tag");
-    titleDiv.append(tagDiv);
-
-
-    h2.innerText = inputTitle.value;
-    pBody.innerText = inputBody.value;
-
-    tagDiv.innerText = "Tags: " + inputTags.value;
-    
-  inputTitle.value = "";
-  inputBody.value = "";
-  inputTags.value = "";
-});
-
 //Renderar Posts
 function renderPosts(posts) {
   for (let i = 0; i < posts.length; i++) {
-   postsArray.push(posts[i]);
+    postsArray.push(posts[i]);
 
     let divContainer = document.createElement("div");
-    divContainer.classList.add("posts");
-    postContainer.append(divContainer);
-
     let titleDiv = document.createElement("div");
-    titleDiv.classList.add("title");
-    divContainer.append(titleDiv);
-
     let h2 = document.createElement("h2");
-    titleDiv.append(h2);
-
     let pBody = document.createElement("p");
-    pBody.classList.add("content");
-    divContainer.append(pBody);
-
     let tagDiv = document.createElement("div");
-    tagDiv.classList.add("div-tag");
-    titleDiv.append(tagDiv);
+    let reactBtn = document.createElement("button");
 
+    divContainer.classList.add("posts");
+    titleDiv.classList.add("title");
+    pBody.classList.add("content");
+    tagDiv.classList.add("div-tag");
+    reactBtn.classList.add("react-btn");
+
+    postContainer.append(divContainer);
+    divContainer.append(titleDiv, pBody, reactBtn);
+    titleDiv.append(h2, tagDiv);
 
     h2.innerText = postsArray[i].title;
-
     pBody.innerText = postsArray[i].body;
-
     tagDiv.innerText = "Tags: " + postsArray[i].tags;
+    reactBtn.innerText = postsArray[i].reactions;
 
+
+    reactBtn.addEventListener("click", function(){
+      postsArray[i].reactions++;
+      reactBtn.innerText = postsArray[i].reactions;
+    });
   }
 }
 
- //Renderar de senaste 5 inläggen under "Latest posts" på vänster sida.
+
+function inputBtnFunc() {
+  //Ifall fälten är tomma skapar den inte en ny post.
+  if (inputTitle.value === "" || inputBody.value === "" || inputTags.value === "") {
+    return;
+  }
+
+  let divContainer = document.createElement("div");
+  let titleDiv = document.createElement("div");
+  let h2 = document.createElement("h2");
+  let pBody = document.createElement("p");
+  let tagDiv = document.createElement("div");
+
+  divContainer.classList.add("posts");
+  titleDiv.classList.add("title");
+  pBody.classList.add("content");
+  tagDiv.classList.add("div-tag");
+
+  postContainer.append(divContainer);
+  divContainer.append(titleDiv, pBody);
+  titleDiv.append(h2, tagDiv);
+
+  h2.innerText = inputTitle.value;
+  pBody.innerText = inputBody.value;
+  tagDiv.innerText = "Tags: " + inputTags.value;
+
+  //Gör så att posten man skapar kommer längst upp
+  postContainer.insertBefore(divContainer, postContainer.firstChild);
+
+  inputTitle.value = "";
+  inputBody.value = "";
+  inputTags.value = "";
+}
+
+//Renderar de senaste 5 inläggen under "Latest posts" på vänster sida.
 function renderFiveLatest(posts) {
   for (let i = 0; i < 5; i++) {
     postsArray.push(posts[i].title);
@@ -96,9 +96,13 @@ function renderFiveLatest(posts) {
     pLatest.innerText = postsArray[i].title;
   }
 }
- 
+
 getPosts(renderPosts);
 getPosts(renderFiveLatest);
+inputBtn.addEventListener("click", inputBtnFunc)
+
+
+
 
 
 
